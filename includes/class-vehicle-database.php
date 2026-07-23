96f8c08e7a74cec41d8119ddce77058fae60b3cd
Thutomofokeng <tatoomofokeng@gmail.com>
2026-07-23 09:06:05 +0000

    Add local vehicle database and VIN decoding framework

--- includes/class-vehicle-database.php
+++ includes/class-vehicle-database.php
@@ -3,14 +3,24 @@
 class CP101_Vehicle_Database {
 
     private $vehicles = [];
+    private $manufacturers = [];
 
     public function __construct() {
 
-        $file = plugin_dir_path(__FILE__) . 'Databases/vehicles.json';
+        // Load known vehicles
+        $vehicle_file = plugin_dir_path(__FILE__) . 'Databases/vehicles.json';
 
-        if (file_exists($file)) {
-            $json = file_get_contents($file);
+        if (file_exists($vehicle_file)) {
+            $json = file_get_contents($vehicle_file);
             $this->vehicles = json_decode($json, true);
+        }
+
+        // Load manufacturer database
+        $manufacturer_file = plugin_dir_path(__FILE__) . 'Databases/manufacturers.json';
+
+        if (file_exists($manufacturer_file)) {
+            $json = file_get_contents($manufacturer_file);
+            $this->manufacturers = json_decode($json, true);
         }
     }
 
@@ -18,10 +28,28 @@ class CP101_Vehicle_Database {
 
         $vin = strtoupper(trim($vin));
 
+        // 1. Exact VIN match
         if (isset($this->vehicles[$vin])) {
             return $this->vehicles[$vin];
         }
 
+        // 2. Manufacturer lookup using WMI
+        $wmi = substr($vin, 0, 3);
+
+        if (isset($this->manufacturers[$wmi])) {
+
+            return [
+                'make'   => $this->manufacturers[$wmi],
+                'model'  => 'Unknown',
+                'year'   => '',
+                'engine' => '',
+                'body'   => '',
+                'drive'  => ''
+            ];
+        }
+
+        // Nothing found
         return null;
     }
+
 }
