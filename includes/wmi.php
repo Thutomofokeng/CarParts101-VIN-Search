2b2b7ab33f15feab03b0ce381ffc6e5dc59ce465
Thutomofokeng <tatoomofokeng@gmail.com>
2026-07-23 16:55:39 +0000

    Implement MINI VIN decoder and prepare Chery database architecture

---
+++ includes/wmi.php
@@ -0,0 +1,80 @@
+{
+    "WMW": "MINI",
+    "WBA": "BMW",
+    "WBS": "BMW M",
+    "WBY": "BMW i",
+
+    "WAU": "Audi",
+    "TRU": "Audi",
+
+    "WVW": "Volkswagen",
+    "WV1": "Volkswagen Commercial",
+    "WVG": "Volkswagen",
+
+    "AA9": "Volkswagen South Africa",
+
+    "WDB": "Mercedes-Benz",
+    "WDC": "Mercedes-Benz",
+    "WDD": "Mercedes-Benz",
+    "W1K": "Mercedes-Benz",
+
+    "SAL": "Land Rover",
+    "SAJ": "Jaguar",
+
+    "JTD": "Toyota",
+    "JT3": "Toyota",
+    "JTB": "Toyota",
+    "AHT": "Toyota South Africa",
+
+    "JTH": "Lexus",
+
+    "JHM": "Honda",
+
+    "JMZ": "Mazda",
+
+    "JSA": "Suzuki",
+
+    "JMB": "Mitsubishi",
+
+    "JN1": "Nissan",
+    "JN8": "Nissan",
+    "MNT": "Nissan South Africa",
+
+    "KMH": "Hyundai",
+
+    "KNA": "Kia",
+
+    "VF1": "Renault",
+
+    "VF3": "Peugeot",
+
+    "VF7": "Citroën",
+
+    "ZFA": "Fiat",
+
+    "ZAR": "Alfa Romeo",
+
+    "MA1": "Mahindra",
+
+    "MAT": "Tata",
+
+    "MPA": "Isuzu",
+
+    "LGW": "GWM",
+
+    "LHB": "Haval",
+
+    "LVV": "Chery",
+
+    "LVT": "Omoda",
+
+    "LTV": "Jaecoo",
+
+    "LC0": "BYD",
+
+    "LSJ": "MG",
+
+    "LNB": "BAIC",
+
+    "LVA": "Foton"
+}
